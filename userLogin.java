bb7d98206ce95d38692842f51b7a66d3ddb8f021
Burak Gozutok <mburakgozutok@outlook.com>
2023-04-29 23:36:55 +0300

    User Login

--- userLogin.java
+++ userLogin.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
+import javax.swing.SortOrder;
+
 public class userLogin{
     public static void main(String[] args) {
         
         String userName, Password, newPassword;
+        
 
         Scanner input = new Scanner(System.in);
 
@@ -12,23 +15,49 @@ public class userLogin{
         
         System.out.print("Enter Password: ");
         Password = input.nextLine();
-
+        
         if(userName.equals("JavaPatikasi") && Password.equals("Java101")){
             System.out.println("Login successful!");
-            
         }
-        else{
-            System.out.println("Incorrect entry!");
-        };
+        else {
+            System.out.println("Login Error!");
+            }
+            
+        if (!Password.equals("Java101")) {
+            System.out.println("Incorrect Password! Would you like to reset your password? (Y/N)");
+            String reply = input.nextLine();
+           
+            if (reply.equals("Y")){
+                System.out.print("Set your new password: ");
+                newPassword = input.nextLine();
 
+                if(newPassword.equals("Java101")){
+                    System.out.print("Could not create password! Please enter another password: ");
+                    newPassword = input.nextLine();
+                    System.out.println("Password successfully created.");
+                }
+            }
+        }
+            
+    }
+}
+
+/*
+        newPassword = input.nextLine();
+ 
         if (!Password.equals("Java101")){
             
             System.out.print("Create new password: ");
-            newPassword = input.nextLine();
-            System.out.println("New password has been created.");
+            
         }
+        
+        else if (newPassword.equals("Java101")){
+            System.out.println("It cannot be the same as your old password.");
+
+        }
+    
         else{
-           
+           System.out.println("New password created.");
         };
-    }
-}
+        
+        */
